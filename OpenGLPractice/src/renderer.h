718fe60f607b76ea9ce69a0b1fb0f2c88d515ec6
Peng Zhou <rdfzzhoupeng@gmail.com>
2021-02-20 19:43:34 +0800

    Initial

---
+++ OpenGLPractice/src/renderer.h
@@ -0,0 +1,16 @@
+#ifndef RENDERER_H_
+#define RENDERER_H_
+
+#include <iostream>
+
+#include <glad/glad.h>
+
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define glCall(x) glClearError();\
+    x;\
+    ASSERT(glLogCall(#x, __FILE__, __LINE__));
+
+void glClearError();
+bool glLogCall(const char* function, const char* file, int line);
+
+#endif // !RENDERER_H_
---
+++ OpenGLPractice/src/vertex_buffer.h
@@ -0,0 +1,11 @@
+#ifndef VERTEX_BUFFER_H_
+#define	VERTEX_BUFFER_H_
+
+class VertexBuffer {
+private:
+	unsigned int rendererId;
+private:
+	
+};
+
+#endif // !VERTEX_BUFFER_H_
